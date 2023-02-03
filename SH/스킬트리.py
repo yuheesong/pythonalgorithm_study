245bbc17ac68149c85d534bd58661aa6d7648a7d
서니 <56826965+kaylee125@users.noreply.github.com>
2023-02-03 20:13:13 +0900

    Update 스킬트리.py

--- SH/스킬트리.py
+++ SH/스킬트리.py
@@ -1,20 +1,16 @@
 def solution(skill, skill_trees):
     answer = -1
-    new_trees=[]
     cnt=0
 
     #skill_trees 각 원소에서 skill에 해당하지 않는 문자 제거 한 값을 새로운 리스트(new_trees)에 추가
     for tree in skill_trees:
         word=''
-        for s in tree:
+        for s in tree: 
             if s in skill:
                 word+=s
-        new_trees.append(word)
-
-    #new_trees 각 원소값 중 skill의 첫번째 문자로 시작하는 값만 count하기
-    for i in new_trees:
-        if skill.startswith(i):
+        if skill.startswith(word): 
             cnt+=1
+
 
     return cnt
 
