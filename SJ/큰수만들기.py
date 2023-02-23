e85ab8bfe39ae486fe960807725375f4bf4c22fb
sunjung-Dev <sun17789@naver.com>
2023-02-23 14:30:16 +0900

    둘만의 암호

--- SJ/큰수만들기.py
+++ SJ/큰수만들기.py
@@ -1,18 +1,31 @@
+# def solution(number, k):
+#     answer = ''
+#     stack = []
+#     for digit in number:
+#         while k > 0 and stack and stack[-1] < digit:
+#             stack.pop()
+#             k -= 1
+#         stack.append(digit)
+#     answer = ''.join(stack[:len(stack)-k])
+#     return answer
+
+# print(solution("4177252841", 4))
+
+
+#stack, pop 
 def solution(number, k):
     answer = ''
-    number = list(number)
-    num = number[0]
-
-
-    for i in range(1, len(number)):
-        if len(number) == len(number) - k:
-            break
-
-        if number[i] > num:
-            pass
-
-
-
+    stack = []
+    for digit in number:
+        while k > 0 and stack and stack[-1] < digit:
+            stack.pop()
+            k -= 1
+        stack.append(digit)
+    answer = ''.join(stack[:len(stack)-k])
     return answer
 
-solution("4177252841", 4)
+print(solution("4177252841", 4))
+
+# 99876 
+
+#zfill -> 몇자리수를 나타내줌 
