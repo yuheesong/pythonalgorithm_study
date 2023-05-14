b7a3554e15bd541e5212a361bd86b98ae39a94d8
sunjung-Dev <sun17789@naver.com>
2023-05-14 17:51:14 +0900

    Delete unnecessary folder

--- SH/없는숫자더하기.py
+++
@@ -1,6 +0,0 @@
-#Lv2-없는 숫자 더하기
-def solution(numbers):
-    
-    return sum([i for i in range(1,10) if i not in numbers])
-
-    
--- SH/하샤드수.py
+++
@@ -1,9 +0,0 @@
-
-def solution(x):
-    sum=0
-    for i in str(x):
-        sum+=int(i)
-    if x%sum==0:
-        return True
-    return False
-solution(10)
